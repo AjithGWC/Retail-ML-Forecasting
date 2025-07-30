a395d8458b8e3e95ff15ae6bf163bdec28e329af
yagash3007 <yagash.kannan@gwcdata.ai>
2025-07-30 11:15:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Retailchart from "./Comonent/chart";
+
+function App() {
+  return (
+    <>
+      <Retailchart />
+    </>
+  );
+}
+
+export default App;
